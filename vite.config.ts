eb6030f66f50e69429e03a3fec40c5c802c3da3c
ozzuworld <eng.ozzu@icloud.com>
2026-01-15 15:37:14 -0500

    1

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL || 'http://localhost:8000',
+        target: process.env.VITE_API_BASE_URL || 'http://VITE_API_BASE_URL:8000',
         changeOrigin: true,
       },
     },
